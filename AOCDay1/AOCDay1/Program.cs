9c81a27431749c80a50e33034b0d6e53289b7b23
Doug Doner <ddoner02@gmail.com>
2019-11-30 21:52:44 -0800

    Day 1 Solution

---
+++ AOCDay1/AOCDay1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AOCDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileText = File.ReadAllText(@"MassInput.txt").Split("\n");
+            Console.WriteLine(fileText[0]);
+
+            double fuelTotal = 0;
+
+            foreach(string massNum in fileText)
+            {
+                fuelTotal += CalcFuelGivenMass(double.Parse(massNum));
+            }
+
+            Console.WriteLine($"Total fuel required for trip: {fuelTotal}");
+        }
+
+
+        public static double CalcFuelGivenMass(double massNumber)
+        {
+            return ((Math.Floor(massNumber / 3)) - 2);
+        }
+    }
+}
